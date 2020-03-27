10ff62ceb15b77bffd789219be226328faaa9d1e
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-03-27 10:55:58 +0300

    switch to redis cloud

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'] }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'] }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
