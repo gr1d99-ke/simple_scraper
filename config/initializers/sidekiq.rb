28ba2ff260be0e50a7d9b56df419a2762c607c10
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-03-27 11:07:18 +0300

    remove sidekiq initializer file

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] }
-end
